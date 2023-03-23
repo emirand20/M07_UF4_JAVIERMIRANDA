6e67f00af23966d48c95943f274b59a4cad011ed
Asus <emirand20>
2023-03-23 19:28:08 +0100

    'hiGotitasDelSaber'

---
+++ GotitasDelSaber/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GotitasdelsaberConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GotitasDelSaber'
--- JaumeBalmes_JavierMiranda/settings.py
+++ JaumeBalmes_JavierMiranda/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'GotitasDelSaber.apps.GotitasdelsaberConfig',
 ]
 
 MIDDLEWARE = [
