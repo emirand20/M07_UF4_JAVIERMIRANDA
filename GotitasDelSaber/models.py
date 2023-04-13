21b412f98821cda1a8249ae68a3f36237a126a61
Asus <emirand20>
2023-04-13 19:24:00 +0200

    createModel

--- GotitasDelSaber/models.py
+++ GotitasDelSaber/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ClaseA(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    surname = models.CharField(max_length=255, null=True)
+    phone = models.CharField(max_length=9, null=True)
+    
+    def __str__(self):
+        return self.name
+    
