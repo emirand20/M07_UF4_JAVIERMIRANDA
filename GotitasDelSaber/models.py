340dd2d8fadce8a54357d1a915a24fbb326936e0
emirand20 <you@jaumebalmes.net>
2023-04-14 12:16:49 +0200

    haciendo pruebas Queries

--- GotitasDelSaber/models.py
+++ GotitasDelSaber/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class ClaseA(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    surname = models.CharField(max_length=255, null=True)
-    phone = models.CharField(max_length=9, null=True)
+    name = models.CharField(max_length=30, null=True)
+    id = models.CharField(max_length=10, null=True)
+    age = models.CharField(max_length=3, null=True)
+    birthplace = models.CharField(max_length=10, null=True)
+    status = models.CharField(max_length=10, null=True)
+    gendre = models.CharField(max_length=10, null=True)
+    bio = models.CharField(max_length=30, null=True)
     
     def __str__(self):
         return self.name
